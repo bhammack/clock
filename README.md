50b31c372fa12a0244d9dde70a5a75a167d6c972
Ben Hammack <hammack.b@gmail.com>
2019-12-07 21:13:05 +0000

    initial testing script

--- README.md
+++ README.md
@@ -1,21 +1,45 @@
 # clock
 Raspberry Pi powered desk clock - integrating a 7 segment display, speaker, and LED ring.
 
+
+## Wiring
+**7-segment**
+V_IO => 3.3V
+VCC => 5V
+GND => GND
+SDA => SDA
+SCL => SCL
+
+**Speaker**
+idk
+
+
+
 ## Setup
 Attempting to revive my old clock project...
 Found this link: https://github.com/adafruit/Adafruit_Python_LED_Backpack
 This seems to be the updated libraries for the LED display
 
+https://learn.adafruit.com/matrix-7-segment-led-backpack-with-the-raspberry-pi/overview
+
+
+
+```
+sudo raspi-config
+# remember to enable i2c!!!
+sudo apt-get install i2c-tools build-essential python-dev python-smbus python-imaging
+```
 
 *from a blank state raspberry pi...*
 ```
 sudo apt-get install python3-pip
+pip3 install adafruit-blinka
+pip3 install adafruit-circuitpython-ht16k33
 ```
 
 
 ```
-sudo apt-get install build-essential python-dev
-sudo apt-get install python-smbus python-imaging
+# This appears to be an older library that doesn't seem to work. The only thing that worked here was dimming the display.
 git clone https://github.com/adafruit/Adafruit_Python_LED_Backpack.git
 cd Adafruit_Python_LED_Backpack
 sudo python3 setup.py install # because 'python' is still 2.7 on the pi...
--- clock.py
+++ clock.py
@@ -1,12 +1,50 @@
-from Adafruit_LED_Backpack import SevenSegment
-# from Adafruit_LED_Backpack import HT16K33
+# https://github.com/adafruit/Adafruit_CircuitPython_HT16K33/blob/master/adafruit_ht16k33/segments.py
+import time
+import board
+import busio
+import pytz
+from adafruit_ht16k33 import segments
+from datetime import datetime
+i2c = busio.I2C(board.SCL, board.SDA)
 
 
+def set_left_colon(display, value):
+    set_top_left_dot(display, value)
+    set_bottom_left_dot(display, value)
+
+def set_top_left_dot(display, value):
+    current = display._get_buffer(0x04)
+    if value:
+        display._set_buffer(0x04, current | 0x04) # top left
+    else:
+        display._set_buffer(0x04, current & ~0x04)
+    display.show()
+
+def set_bottom_left_dot(display, value):
+    current = display._get_buffer(0x04)
+    if value:
+        display._set_buffer(0x04, current | 0x08) # bottom left
+    else:
+        display._set_buffer(0x04, current & ~0x08)
+    display.show()
 
 
 
 def main():
-    print("Hello boys, I'm back!")
+    print("Clock started...")
+
+    display = segments.BigSeg7x4(i2c)
+    display.fill(0)
+    
+    while True:
+        now = datetime.now(tz=pytz.timezone('America/New_York'))
+        hours = now.strftime('%-I').rjust(2)
+        minutes = now.strftime('%M')
+        display.print(hours + ':' + minutes)
+        set_top_left_dot(display, now.strftime('%p') == 'AM')
+        set_bottom_left_dot(display, now.strftime('%p') == 'PM')
+        time.sleep(60 - time.time() % 60)
+
 
 if __name__ == '__main__':
     main()
